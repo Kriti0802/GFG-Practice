c79b7bdc0c4a3bad0b90d740216ebc05c058298d
Kriti Singh <90671322+Kriti0802@users.noreply.github.com>
2022-04-04 12:37:53 +0530

    Add files via upload

---
+++ delete_middle_element_from_stack.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+class Solution
+{
+    
+    public:
+    void solve(stack<int>&s,int count,int size){
+    if(count==size/2){
+       
+        s.pop();
+        return;
+    }
+    int num=s.top();
+    s.pop();
+    
+    //recursively
+    solve(s,count+1,size);
+    s.push(num);
+}
+
+    //Function to delete middle element of a stack.
+    void deleteMid(stack<int>&s, int sizeOfStack)
+    {
+        // code here.. 
+        int count=0;
+        solve(s,count,sizeOfStack);
+    }
+};
