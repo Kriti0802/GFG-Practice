8ef6aaef7789d20b44d824c6f7b56a87f5901669
Kriti Singh <90671322+Kriti0802@users.noreply.github.com>
2022-04-10 14:59:09 +0530

    Add files via upload

---
+++ find_middle_element_in_linkedlist.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+void printList(Node* node) 
+{ 
+    while (node != NULL) { 
+        cout << node->data <<" "; 
+        node = node->next; 
+    }  
+    cout<<"\n";
+} class Solution{
+    public:
+    /* Should return data of middle node. If linked list is empty, then  -1*/
+    int getMiddle(Node *head)
+    {
+        // Your code here
+        Node* fast=head;
+        Node* slow=head;
+        while(fast!=NULL && fast->next!=NULL&& slow!=NULL)
+        {
+            fast=fast->next->next;
+            slow=slow->next;
+        }
+        return slow->data;
+    }
+};
